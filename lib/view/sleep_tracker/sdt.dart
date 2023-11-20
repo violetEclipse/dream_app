5eb6cbfdaee2148c8aa651875cd3ce67b47f9a17
violetEclipse <applebits23@gmail.com>
2023-11-20 16:43:36 +0200

    asad

---
+++ lib/view/sleep_tracker/sdt.dart
@@ -0,0 +1,54 @@
+
+import 'package:flutter/material.dart';
+
+import '../../common/colo_extension.dart';
+import '../../common_widget/round_button.dart';
+import '../../common_widget/setting_row.dart';
+import '../../common_widget/title_subtitle_cell.dart';
+import 'package:animated_toggle_switch/animated_toggle_switch.dart';
+
+class ProfileView extends StatefulWidget {
+  const ProfileView({super.key});
+
+  @override
+  State<ProfileView> createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  bool positive = false;
+
+  List accountArr = [
+    {"image": "assets/img/p_personal.png", "name": "Personal Data", "tag": "1"},
+    {"image": "assets/img/p_achi.png", "name": "Achievement", "tag": "2"},
+    {
+      "image": "assets/img/p_activity.png",
+      "name": "Activity History",
+      "tag": "3"
+    },
+    {
+      "image": "assets/img/p_workout.png",
+      "name": "Workout Progress",
+      "tag": "4"
+    }
+  ];
+
+  List otherArr = [
+    {"image": "assets/img/p_contact.png", "name": "Contact Us", "tag": "5"},
+    {"image": "assets/img/p_privacy.png", "name": "Privacy Policy", "tag": "6"},
+    {"image": "assets/img/p_setting.png", "name": "Setting", "tag": "7"},
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+      backgroundColor: TColor.white,
+      
+    );
+  }
+}
+
+
+
+
+git config --global user.email "applebits23@gmail.com"
+  git config --global user.name "violetEclipse"
